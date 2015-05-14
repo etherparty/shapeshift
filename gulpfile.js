a0ab130eefe976a629972bbd832b40a0ad1cc24c
Martijn Buurman <martijn@m19.nl>
2015-05-14 19:52:23 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  js: [
+    './public/app/bower_components/angular/angular.js',
+    './public/app/bower_components/angular-bootstrap/ui-bootstrap.min.js',
+    './public/app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+    './public/app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+    './public/app/app.js',
+    './public/app/components/controllers/*.js',
+    './public/app/components/helpers/*.js',
+    './public/app/components/filters/*.js',
+    './public/app/components/services/*.js',
+    './public/app/components/directives/*.js'
+  ]
+};
+
+gulp.task('js', function () {
+  gulp.src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/dist'))
+});
+
+gulp.task('default', ['js'], function () {
+  gulp.watch(paths.js, ['js']);
+});
---
+++ public/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('ethershift', [
+  'ui.router',
+  'ui.bootstrap'
+])
+  .run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  }])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/components/templates/home.html'
+      });
+  });
