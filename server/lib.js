c697f402953b4446604541e9d3428f6c43398b93
Martijn Buurman <martijn@m19.nl>
2015-05-18 20:49:09 +0200

    Added sending ether

--- server/lib.js
+++ server/lib.js
@@ -2,6 +2,8 @@ var crypto = require('crypto');
 var axios = require('axios');
 var Promise = require('bluebird');
 var BigNumber = require('bignumber.js');
+var web3 = require('web3');
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 var blockchainID = process.env.ETHERSHIFT_BLOCKCHAIN_ID;
 var blockchainPassword1 = process.env.ETHERSHIFT_BLOCKCHAIN_PASSWORD_1;
@@ -25,9 +27,19 @@ var lib = {
 
     return new BigNumber(btc).times(rate).toNumber();
   },
-  sendEther: function (amoun, address) {
+  sendEther: function (amount, address) {
     return new Promise(function (resolve, reject) {
-      resolve(true);
+      web3.eth.sendTransaction({
+        from: web3.eth.coinbase,
+        to: address,
+        value: web3.toWei(amount, 'ether')
+      }, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
     });
   }
 };
